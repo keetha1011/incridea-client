de753eb7b32b0a8dd0c07154c18301e1b5c0c82c
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-11-23 21:59:58 +0530

    fix: backend-url

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -66,7 +66,10 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
-const uri = "https://incridea-pai3.onrender.com/graphql";
+const uri =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/graphql"
+    : "https://incridea-pai3.onrender.com/graphql";
 const sseLink = new SSELink({ uri });
 const httpLink = new HttpLink({ uri });
 
