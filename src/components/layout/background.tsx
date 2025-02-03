793415b4ed14ea309ef9c8888b779cf19dea93b7
Pratham A Kadekar <pratham20442@gmail.com>
2025-02-03 14:26:47 +0530

    [UI] backkground

--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import Particles from "react-tsparticles";
+import { particlesConfig } from "./particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
 
 export default function BackGroundGradient({
   children,
@@ -8,17 +12,28 @@ export default function BackGroundGradient({
 }) {
   const bgRef = useRef<HTMLDivElement>(null);
 
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await new Promise((resolve) => resolve());
+    },
+    [],
+  );
+
   useEffect(() => {
     if (bgRef.current) {
       gsap.fromTo(
         bgRef.current,
         {
           background:
-            "linear-gradient(45deg, rgba(0,51,31,1) 10%, rgba(0,102,63,1) 20%, rgba(0,51,31,1) 90%)",
+            "linear-gradient(40deg , rgba(0,51,31,1) 10%, rgba(0,102,63,1) 30%, rgba(0,51,31,1) 90%)",
         },
         {
           background:
-            "linear-gradient(45deg, rgba(0,51,31,1) 10%, rgba(0,102,63,1) 80%, rgba(0,51,31,1) 90%)",
+            "linear-gradient(40deg , rgba(0,51,31,1) 10%, rgba(0,102,63,1) 70%, rgba(0,51,31,1) 90%)",
           repeat: -1,
           yoyo: true,
           duration: 6,
@@ -29,7 +44,17 @@ export default function BackGroundGradient({
   }, []);
 
   return (
-    <div ref={bgRef} className="min-w-screen min-h-screen bg-[rgb(0,51,31)]">
+    <div
+      ref={bgRef}
+      className="w-screen min-h-screen bg-[rgb(0,51,31)] backdrop-blur-3xl"
+    >
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={particlesConfig}
+        loaded={particlesLoaded}
+        className="z-10"
+      />
       {children}
     </div>
   );
