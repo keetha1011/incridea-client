a3ee65eb8922e4462fb943ddf93fee02f702ab9c
Pratham A Kadekar <pratham20442@gmail.com>
2025-02-05 09:04:32 +0530

    [feat/pending] profile page

--- src/pages/profile/UserEvents.tsx
+++ src/pages/profile/UserEvents.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@apollo/client";
 import Link from "next/link";
-import { type FC } from "react";
+import { useEffect, type FC } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
 import EventCard from "./Event";
+import Image from "next/image";
 
 import Spinner from "~/components/spinner";
 import { RegisterdEventsDocument } from "~/generated/generated";
@@ -14,70 +15,35 @@ const UserEvents: FC<{
 }> = ({ userId }) => {
   const { data: events, loading, error } = useQuery(RegisterdEventsDocument);
 
+  useEffect(() => {
+    console.log(events?.registeredEvents.data);
+  }, [events]);
+
   return (
-    <section className="h-full lg:overflow-y-scroll rounded-xl border border-green-700 bg-green-700 p-3 md:p-6">
-      <LeaderBoard />
-      <div className="titleFont py-5 mt-4 text-center text-5xl text-white">
-        My Exploration
-      </div>
-
-      <div className="mt-2 flex items-center justify-center">
-        {loading ? (
-          <div className="flex h-full items-center justify-center">
-            <Spinner size={"medium"} intent={"white"} />
+    <section className="w-full h-full flex flex-col items-center justify-center p-4">
+      <div className="w-full h-full flex flex-col flex-nowrap border-2 rounded-lg bg-gradient-to-b from-primary-950/70 via-primary-800/70 to-primary-950/70 backdrop-blur-sm border-secondary-900/50">
+        <div className="text-white">
+          <h1 className="text-center md:text-6xl text-4xl mt-6 font-bold">
+            My Quests
+          </h1>
+          <div className="mx-auto border-2 border-secondary-900/50 w-fit rounded-full p-1 px-2 mt-4">
+            <span className="text-lg font-semibold text-secondary-600">
+              {5}
+            </span>{" "}
+            <span className="opacity-70">Quests left of</span>{" "}
+            <span className="text-lg font-semibold text-secondary-600">
+              {5}
+            </span>
           </div>
-        ) : events?.registeredEvents.__typename ===
-            "QueryRegisteredEventsSuccess" &&
-          events.registeredEvents.data.length === 0 ? (
-          <div
-            data-scroll
-            className={`flex h-full flex-col items-center justify-center gap-5 rounded-xl p-10 text-center text-xl text-white`}
-          >
-            <MdOutlineExplore size={50} />
-            <p className="text-base text-white/80 md:text-xl">
-              Register for an event to see it here!
-            </p>
-            <Link href="/events" className="text-white">
-              <button className="mt-1 flex w-full shrink-0 items-center justify-center gap-2 rounded-full bg-gradient-to-tr from-secondary-800 to-secondary-600 px-5 py-2 text-sm capitalize text-white transition-all duration-300 hover:scale-[1.02] hover:brightness-125 md:text-lg">
-                <FaExternalLinkAlt size={16} />
-                Explore Quests
-              </button>
-            </Link>
-          </div>
-        ) : null}
-
-        {!loading &&
-          !error &&
-          events?.registeredEvents.__typename ===
-            "QueryRegisteredEventsSuccess" &&
-          events.registeredEvents.data.length !== 0 && (
-            <div className="space-y-2 w-full">
-              <div className="mx-auto w-full space-y-4 px-5">
-                <div className="z-1 mb-5 flex w-full justify-center text-center font-bold text-white">
-                  <p className="w-fit rounded-full border border-primary-200/80 px-3 py-1">
-                    You have entered{" "}
-                    <span className="text-secondary-500">
-                      {events.registeredEvents.data.length}
-                    </span>{" "}
-                    quest{events.registeredEvents.data.length > 1 && "s"}!
-                  </p>
-                </div>
-
-                <div className="flex flex-wrap items-stretch justify-center gap-5 pb-5">
-                  {events?.registeredEvents.__typename ===
-                    "QueryRegisteredEventsSuccess" &&
-                    events?.registeredEvents.data?.map((event, i) => (
-                      <EventCard
-                        key={i}
-                        teams={event.teams}
-                        event={event}
-                        userId={userId}
-                      />
-                    ))}
-                </div>
-              </div>
-            </div>
-          )}
+        </div>
+        <div className="w-full h-full">
+          {/* cards */}
+          {/* {events?.registeredEvents.data.map((event, idx) => {
+            return (
+              <Image src="/test.jpg" alt="img" key={idx} className="w-[20vw]" width={100} height={100}/>
+            )
+          })} */}
+        </div>
       </div>
     </section>
   );
